4cfd6b220a6474d03d74cd619af75bb917420190
Mia Quintana <mia.quintana@live.com>
2024-04-26 22:34:03 -0400

    done! just need to update projects

--- app/public/wp-content/themes/mytheme/index.php
+++ app/public/wp-content/themes/mytheme/index.php
@@ -24,15 +24,24 @@
 
 
   <div class="container gallery">
-    <div class="row flex-lg-row align-items-center g-5 py-5 m-0">
-      <div class="col-10 col-lg-4 gallery-item container" style="background-image: url('<?php echo get_theme_file_uri('/assets/images/studying.png'); ?>');">
-      <p class="lora caption">This text will come up on hover</p>
+    <div class="row flex-lg-row align-items-center g-5 py-5 m-0 wrapper">
+      <div class="col-10 col-lg-4 gallery-item center" style="background-image: url('<?php echo get_theme_file_uri('/assets/images/studying.png'); ?>');">
+        <div class="caption">
+          <p class="lora px-5">Constantly studying to stay up-to-date with trends in web development.</p>
+          <a href="<?php echo site_url('/projects'); ?>">View Projects</a>
+        </div>
       </div>
       <div class="col-10 col-lg-4 center gallery-item" style="background-image: url('<?php echo get_theme_file_uri('/assets/images/pensive.jpg'); ?>');">
-        <p class="lora caption">This text will come up on hover</p>
+        <div class="caption">
+          <p class="lora px-5">Highly motivated and focused on improvement.</p>
+          <a href="<?php echo site_url('/resume'); ?>">View Resume</a>
+        </div>
       </div>
       <div class="col-10 col-lg-4 center gallery-item" style="background-image: url('<?php echo get_theme_file_uri('/assets/images/cool-pic.png'); ?>');">
-        <p class="lora caption">This text will come up on hover</p>
+        <div class="caption">
+          <p class="lora px-5">Gaining experinece building aesthetically pleasing and intuitive web pages.</p>
+          <a href="">Latest Project</a>
+        </div>
       </div>
     </div>
   </div>
@@ -40,35 +49,23 @@
 
   <div class="container col-xxl-8" id ="latest-project">
     <h1 class="fw-bold italiana-regular my-2 center">Latest Project</h1>
-    <div class=" row flex-lg-row align-items-center g-5 m-0" >
+    <div class="row flex-lg-row g-5 m-0" >
       <div class="col-lg-6 px-5 right">
-        <h3 class=" italiana-regular fw-bold my-2">My Sister's Website</h3>
-        <p class="lora">A WordPress site made with lots of love.</p>
-        <a href="<?php echo site_url('/projects'); ?>">View More Projects</a>
+        <div class="row-6 py-4">
+          <a href="" target="_blank" rel="noopener noreferrer"><h3 class=" italiana-regular fw-bold my-2 btn-mine">My Fiance's Website</h3></a>
+          <p class="lora">A prototype for a WordPress site made with lots of love.</p>
+        </div>
+        <div class="row-6 py-5">
+          <a href="<?php echo site_url('/projects'); ?>" class="btn-mine" >View More Projects</a> 
+        </div>
+        
       </div>
 
       <div class="col-lg-6 px-5 center"> 
         <a href="" target="_blank" rel="noopener noreferrer"><img class="bd-placeholder-img my-3" width="350" height="240" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src="<?php echo get_theme_file_uri('assets/images/keboard.png')?>"><rect width="100%" height="100%" fill="var(--bs-secondary-color)" ></rect></img></a>
       </div>
     </div>
-  </div>
-                <!-- <div class="col-lg-6 px-5">
-                <a href="https://mia-q.github.io/recipe-search-app/" target="_blank" rel="noopener noreferrer"><img class="bd-placeholder-img rounded my-3 " width="250" height="140" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" style="border: 1px solid black;" src=<?php echo get_theme_file_uri('/assets/images/recipe-search.png');?>><rect width="100%" height="100%" fill="var(--bs-secondary-color)" ></rect></img><h2 class="italiana-regular fw-bold">Recipe Search App</h2></a>
-                <p class="lora">Built in collaboration with a friend from my coding bootcamp (thanks, Evan), this recipe search app was made from scratch with only Vanilla Javascript. It is imperfect and our styling choices are questionable (thanks again, Evan), but this project solidified basic JS concepts for me. Utilizes spoonacular API.</p>
-                </div> -->
-                <!-- <div class="col-lg-6 px-5">
-                  <a href="https://mia-q.github.io/ContactKeeper/index.html" target="_blank" rel="noopener noreferrer"><img class="bd-placeholder-img rounded my-3" width="250" height="140" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" src=<?php echo get_theme_file_uri('/assets/images/contact.png');?> style="border: 1px solid black;"><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></img> <h2 class="fw-bold italiana-regular">ContactKeeper</h2></a>
-                    <p class="lora">I enjoyed learning React and wanted to create something without a tutorial or guide. Though not an uncommon project, I chose a contact book because I wanted to be able to create something using what I had learned through other, smaller React projects I had completed. The process for creating a contact book was straightforward enough for me to complete on my own. This is the result.<p>
-                </div>
-                  <div class="col-lg-6 px-5">
-                  <a href="https://github.com/mia-q/nucampsite-server" target="_blank" rel="noopener noreferrer" ><img class="bd-placeholder-img rounded my-3" width="250" height="140" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false" style="border: 1px solid black;" src=<?php echo get_theme_file_uri('/assets/images/express-server.png');?>><rect width="100%" height="100%" fill="var(--bs-secondary-color)" ></rect></img><h2 class="fw-bold italiana-regular">Express Server Practice</h2></a>
-                  <p class="lora"> Not a project I created on my own, but demonstrative of the extent of my knowledge related to backend development. This project was a major part of the coursework during my bootcamp and introduced me to express, mongoose, mongoDB, and related web technologies. My current focus is front-end development but I am familiar with backend basics.</p>
-                  </div>
-               -->
-
-            
-          
-
+</div>
           
 
 <?php get_footer(); ?>
